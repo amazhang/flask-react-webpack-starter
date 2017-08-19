da20856c9b7ce13ff7e05f8c7feae198899c5d30
Anthony Zhang <azhang102@gmail.com>
2017-08-19 13:55:20 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    "./client/main.jsx"
+  ],
+  output: {
+    path: __dirname + '/static',
+    publicPath: "/assets/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        },
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+  ]
+};
